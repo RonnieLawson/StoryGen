367c6f525c87115cbf9ffa4278b614676f76edde
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-03-09 16:36:38 +0000

    initial commit

---
+++ CharacterGen/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterGen
+{
+    public class CharacterGen
+    {
+    }
+}
