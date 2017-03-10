1e16d23bef79313e50120552443b6e0d177cd19e
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-03-10 09:41:30 +0000

    added first test

--- CharacterGen.Tests/Class1.cs
+++ CharacterGen.Tests/Class1.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CharacterGen.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class CharacterGenTests
     {
+        [TestFixture]
+        public class GivenACharacterGen
+        {
+            private CharacterGen _characterGen;
+
+            [OneTimeSetUp]
+            public void WhenCreating()
+            {
+                _characterGen = new CharacterGen();
+            }
+
+            [Test]
+            public void ThenTheRightObjectIsCreated()
+            {
+                Assert.That(_characterGen.GetType(), Is.EqualTo(typeof(CharacterGen)));
+            }
+        }
     }
 }
